d559c692ae77131049e099b93ac77a6a31fec421
Lucas Chapuis <l.chapuis@patriarche.fr>
2022-11-10 13:27:10 +0100

    feat: add uniformed InputSelect

--- src/components/io/AppInputSelect.vue
+++ src/components/io/AppInputSelect.vue
@@ -1,33 +1,52 @@
 <template>
-  <label
+  <div
     v-theme="{ scheme }"
-    class="mk-AppInpupSelect"
+    class="mk-AppInputSelect"
+    :data-focus="focus || undefined"
+    :data-fill="props.fill || undefined"
   >
-    <select
-      :name="props.name"
-      @input="handleChange"
-    >
-      <option
-        v-for="(option, index) in props.options"
-        :key="index"
-        :value="index"
-        :selected="isSelectedOption(option.value)"
-      >
-        {{ option.label }}
-      </option>
-
-    </select>
+    <label>
+      <AppInputLabel v-if="props.label">
+        {{ props.label }}
+      </AppInputLabel>
+      <div class="mk-AppInputSelect-input">
+        <select
+          :name="props.name"
+          @input="handleChange"
+          @focus="onFocus"
+          @blur="onBlur"
+        >
+          <option
+            v-for="(option, index) in props.options"
+            :key="index"
+            :value="index"
+            :selected="isSelectedOption(option.value)"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+        <AppIcon icon="expand_more" />
+      </div>
+    </label>
+    <AppInputHint v-if="props.hint">
+      {{ props.hint }}
+    </AppInputHint>
     <!-- <AppInputError v-if="state.error">
       {{ state.error }}
     </AppInputError> -->
-  </label>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue';
 import isEqual from 'lodash/isEqual';
-import type { InputState, ValidateInput } from '@src/definition';
+import type {
+  InputState, ValidateInput,
+} from '@src/definition';
 // import AppInputError from '@src/components/io/decoration/AppInputError.vue';
+import AppInputHint from '@src/components/io/decoration/AppInputHint.vue';
+import AppInputLabel from '@src/components/io/decoration/AppInputLabel.vue';
+import AppIcon from '@src/components/AppIcon.vue';
 import useInput from '@src/composables/useInput';
 import useComponentTheme from '@src/composables/useComponentTheme';
 
@@ -37,6 +56,9 @@ type Props = {
   modelValue: InputState<Value>;
   name?: string;
   validate?: ValidateInput<Value>;
+  label?: string;
+  hint?: string;
+  fill?: boolean;
   options: {
     label: string;
     value: Value;
@@ -52,7 +74,9 @@ const emits = defineEmits<Emits>();
 
 const { scheme } = useComponentTheme();
 
-const { onChange, state } = useInput<Value>({
+const {
+  onChange, onFocus, onBlur, state, focus,
+} = useInput<Value>({
   props: computed(() => props),
   emits,
 });
@@ -72,3 +96,64 @@ function handleChange(evt: Event) {
   onChange(newOption.value);
 }
 </script>
+
+<style lang="scss">
+.mk-AppInputSelect {
+    --mk-input-select-padding-x-left: var(--app-input-padding-x);
+    --mk-input-select-padding-x-right: calc(var(--app-input-padding-x) * 2);
+    --mk-input-select-padding-y: var(--app-input-padding-y);
+    --mk-input-select-border-radius: var(--app-border-radius);
+    --mk-input-select-background-color: var(--app-input-background-color);
+    --mk-input-select-border-color: var(--app-input-border-color);
+
+    display: inline-block;
+
+    select {
+        width: 100%;
+        padding:
+            var(--mk-input-select-padding-y)
+            var(--mk-input-select-padding-x-right)
+            var(--mk-input-select-padding-y)
+            var(--mk-input-select-padding-x-left);
+        background-color: var(--mk-input-select-background-color);
+        border: 1px solid var(--mk-input-select-border-color);
+        border-radius: var(--mk-input-select-border-radius);
+        outline: none;
+        transition: border-color var(--app-transition-duration-border);
+        appearance: none;
+    }
+
+    &-input {
+        position: relative;
+
+        .mk-AppIcon {
+            --mk-icon-size: 24px;
+
+            position: absolute;
+            top: 50%;
+            right: calc((var(--mk-input-select-padding-x-right) - var(--mk-icon-size)) / 2);
+            font-weight: 200;
+            pointer-events: none;
+            transform: translate(0, -50%);
+        }
+    }
+
+    &[data-focus="true"] {
+        --mk-input-select-border-color: var(--app-primary-color);
+    }
+
+    &[data-fill="true"] {
+        display: block;
+    }
+
+    .mk-AppInputLabel {
+        display: block;
+        margin-bottom: var(--app-m-1);
+    }
+
+    .mk-AppInputHint {
+        display: block;
+        margin-top: var(--app-m-1);
+    }
+}
+</style>
