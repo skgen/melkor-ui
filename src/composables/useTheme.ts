eac4fcd7f7ab0134e14e85d867689ae7e9eea35a
Lucas Chapuis <l.chapuis@patriarche.fr>
2022-12-07 16:45:52 +0100

    feat: provide a method to update theme

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -1,10 +1,17 @@
 import { inject } from 'vue';
 import { themeContextKey } from '@src/definition';
+import { useThemeStore } from '@src/stores/theme';
 
 export default function useTheme() {
   const injectedTheme = inject(themeContextKey);
+  const themeStore = useThemeStore();
+
+  function updateTheme(newTheme: string) {
+    themeStore.updateTheme(newTheme);
+  }
 
   return {
     theme: injectedTheme,
+    updateTheme,
   };
 }
