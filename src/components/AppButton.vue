19e391e6fa27ea00b526c0247f95743059c5a3a5
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-03-24 16:36:35 +0100

    feat: enhance AppButton with wide and light props

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -3,6 +3,7 @@
     v-theme="theme"
     class="mk-AppButton"
     :data-type="type"
+    :data-size="size"
     :data-on-primary="onPrimary"
     :disabled="disabled"
   >
@@ -19,6 +20,8 @@ type Props = {
   disabled?: boolean;
   onPrimary?: boolean;
   text?: boolean;
+  light?: boolean;
+  wide?: boolean;
 };
 
 const props = defineProps<Props>();
@@ -32,8 +35,19 @@ const type = computed(() => {
   if (props.outlined) {
     return 'outlined';
   }
+  if (props.light) {
+    return 'light';
+  }
   return 'plain';
 });
+
+const size = computed(() => {
+  if (props.wide) {
+    return 'wide';
+  }
+  return undefined;
+});
+
 const onPrimary = computed(() => props.onPrimary || undefined);
 const disabled = computed(() => props.disabled || undefined);
 
@@ -45,12 +59,11 @@ const disabled = computed(() => props.disabled || undefined);
 .mk-AppButton {
     --mk-button-padding-x: var(--app-m-2);
     --mk-button-padding-y: var(--app-m-1);
-    --mk-button-bordered-padding-x: calc(var(--mk-button-padding-x) - var(--mk-button-border-width));
-    --mk-button-bordered-padding-y: calc(var(--mk-button-padding-y) - var(--mk-button-border-width));
     --mk-button-spacing-content: var(--app-m-1);
     --mk-button-background-color: var(--app-primary-color);
     --mk-button-text-color: var(--app-text-color-on-primary);
     --mk-button-hollowed-text-color: var(--app-primary-color);
+    --mk-button-light-text-color: var(--app-text-color);
     --mk-button-border-color: var(--app-primary-color);
     --mk-button-border-width: 1px;
     --mk-button-border-radius: var(--app-border-radius);
@@ -58,6 +71,25 @@ const disabled = computed(() => props.disabled || undefined);
     --mk-button-font-size: var(--app-font-size);
     --mk-button-line-height: var(--app-line-height);
     --mk-button-font-weight: 500;
+
+    // Bordered
+    --mk-button-bordered-padding-x: calc(var(--mk-button-padding-x) - var(--mk-button-border-width));
+    --mk-button-bordered-padding-y: calc(var(--mk-button-padding-y) - var(--mk-button-border-width));
+
+    // Wide
+    --mk-button-wide-font-size: calc(var(--app-font-size) * 1.2);
+    --mk-button-wide-bordered-padding-x: calc(var(--mk-button-wide-padding-x) - var(--mk-button-border-width));
+    --mk-button-wide-bordered-padding-y: calc(var(--mk-button-wide-padding-y) - var(--mk-button-border-width));
+    --mk-button-wide-padding-x: calc(var(--app-m-2) * 1.5);
+    --mk-button-wide-padding-y: calc(var(--app-m-1) * 1.5);
+
+    @include light {
+        --mk-button-light-background-color: rgb(var(--app-primary-tone-color) / 0.1);
+    }
+
+    @include dark {
+        --mk-button-light-background-color: rgb(var(--app-primary-tone-color) / 0.35);
+    }
 
     display: inline-flex;
     gap: var(--mk-button-spacing-content);
@@ -72,15 +104,27 @@ const disabled = computed(() => props.disabled || undefined);
     border-width: var(--mk-button-border-width);
     border-radius: var(--mk-button-border-radius);
 
-    &[data-type="plain"] {
-        color: var(--mk-button-text-color);
-        background-color: var(--mk-button-background-color);
+    &[data-size="wide"] {
+        padding: var( --mk-button-wide-padding-y) var(--mk-button-wide-padding-x);
+        font-size: var(--mk-button-wide-font-size);
+    }
 
+    &[data-type="plain"], &[data-type="light"] {
         &[data-on-primary="true"] {
             color: var(--app-primary-color);
             background-color: var(--app-background-color-on-primary);
             border-color: var(--app-background-color-on-primary);
         }
+    }
+
+    &[data-type="plain"] {
+        color: var(--mk-button-text-color);
+        background-color: var(--mk-button-background-color);
+    }
+
+    &[data-type="light"] {
+        color: var(--mk-button-light-text-color);
+        background-color: var(--mk-button-light-background-color);
     }
 
     &[data-type="outlined"],
@@ -96,6 +140,10 @@ const disabled = computed(() => props.disabled || undefined);
     &[data-type="outlined"] {
         padding: var(--mk-button-bordered-padding-y) var(--mk-button-bordered-padding-x);
         border-style: solid;
+
+        &[data-size="wide"] {
+            padding: var(--mk-button-wide-bordered-padding-y) var(--mk-button-wide-bordered-padding-x);
+        }
 
         &[data-on-primary="true"] {
             border-color: var(--app-background-color-on-primary);
