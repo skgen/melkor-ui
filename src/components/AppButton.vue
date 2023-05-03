f9a936fa71a14b629fd6cd111c8855bf95e4dd91
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-05-03 15:34:12 +0200

    feat: AppButton v2

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -5,9 +5,12 @@
     :data-type="type"
     :data-size="size"
     :data-on-primary="onPrimary"
+    :data-is-active="isActive"
     :disabled="disabled"
   >
-    <slot />
+    <div class="mk-AppButton-content">
+      <slot />
+    </div>
   </button>
 </template>
 
@@ -19,6 +22,7 @@ type Props = {
   outlined?: boolean;
   disabled?: boolean;
   onPrimary?: boolean;
+  active?: boolean;
   text?: boolean;
   light?: boolean;
   wide?: boolean;
@@ -50,6 +54,7 @@ const size = computed(() => {
 
 const onPrimary = computed(() => props.onPrimary || undefined);
 const disabled = computed(() => props.disabled || undefined);
+const isActive = computed(() => props.active || undefined);
 
 </script>
 
@@ -57,98 +62,380 @@ const disabled = computed(() => props.disabled || undefined);
 @import "@style/mixins";
 
 .mk-AppButton {
+    // Config variables
     --mk-button-padding-x: var(--app-m-2);
     --mk-button-padding-y: var(--app-m-1);
     --mk-button-spacing-content: var(--app-m-1);
-    --mk-button-background-color: var(--app-primary-color);
-    --mk-button-text-color: var(--app-text-color-on-primary);
-    --mk-button-hollowed-text-color: var(--app-primary-color);
-    --mk-button-light-text-color: var(--app-text-color);
-    --mk-button-border-color: var(--app-primary-color);
+    --mk-button-main-tone-color: var(--app-primary-tone-color);
+    --mk-button-text-color: var(--app-text-color);
+    --mk-button-text-color-on-main-color: var(--app-text-color-on-primary);
+    --mk-button-border-color: var(--app-border-color);
     --mk-button-border-width: 1px;
     --mk-button-border-radius: var(--app-border-radius);
     --mk-button-cursor: pointer;
+    --mk-button-icon-color: var(--app-text-color-soft);
     --mk-button-font-size: var(--app-font-size);
     --mk-button-line-height: var(--app-line-height);
     --mk-button-font-weight: 500;
 
-    // Wide
+    // Computed variables
+    --mk-button-bordered-padding-x: calc(var(--mk-button-padding-x) - var(--mk-button-border-width));
+    --mk-button-bordered-padding-y: calc(var(--mk-button-padding-y) - var(--mk-button-border-width));
     --mk-button-wide-font-size: calc(var(--app-font-size) * 1.2);
-    --mk-button-wide-line-height: calc(var(--app-font-size) * 1.2);
-    --mk-button-wide-padding-x: calc(var(--app-m-2) * 1.5);
-    --mk-button-wide-padding-y: calc(var(--app-m-1) * 1.5);
+    --mk-button-wide-padding-x: var(--mk-button-padding-x);
+    --mk-button-wide-padding-y: calc(var(--mk-button-padding-y) * 1.5);
+    --mk-button-main-color: rgb(var(--mk-button-main-tone-color));
 
-    @include light {
-        --mk-button-light-background-color: rgb(var(--app-primary-tone-color) / 0.1);
-    }
+    $this: &;
 
-    @include dark {
-        --mk-button-light-background-color: rgb(var(--app-primary-tone-color) / 0.35);
-    }
-
-    display: inline-flex;
-    gap: var(--mk-button-spacing-content);
-    align-items: center;
-    justify-content: center;
-    padding: var(--mk-button-padding-y) var(--mk-button-padding-x);
+    position: relative;
+    padding: 0;
+    overflow: hidden;
     font-size: var(--mk-button-font-size);
     font-weight: var(--mk-button-font-weight);
     line-height: var(--mk-button-line-height);
     cursor: var(--mk-button-cursor);
-    border-color: var(--mk-button-border-color);
-    border-style: solid;
-    border-width: var(--mk-button-border-width);
-    border-radius: var(--mk-button-border-radius);
+    border: none;
+    transition:
+        opacity var(--app-transition-duration-background),
+        color var(--app-transition-duration-background);
 
-    &[data-size="wide"] {
-        padding: var(--mk-button-wide-padding-y) var(--mk-button-wide-padding-x);
-        font-size: var(--mk-button-wide-font-size);
+    @mixin icon-color() {
+        .mk-AppIcon {
+            transition: color var(--app-transition-duration-background);
+
+            --mk-icon-color: var(--mk-button-icon-color);
+        }
+    }
+
+    @mixin on-primary() {
+        &[data-on-primary="true"] {
+            @content;
+        }
+    }
+
+    @mixin on-classic() {
+        &:not([data-on-primary="true"]) {
+            @content;
+        }
+    }
+
+    @mixin is-active() {
+        &[data-is-active="true"] {
+            @content;
+        }
+    }
+
+    @mixin content() {
+        #{$this} {
+            &-content {
+                @content;
+            }
+        }
+    }
+
+    &-content {
+        position: relative;
+        display: flex;
+        gap: var(--mk-button-spacing-content);
+        align-items: center;
+        justify-content: center;
+        padding: var(--mk-button-padding-y) var(--mk-button-padding-x);
+        border-radius: var(--mk-button-border-radius);
+        transition: border-color var(--app-transition-duration-background);
+    }
+
+    &::before {
+        @include pseudo;
+
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        user-select: none;
+        border-radius: var(--mk-button-border-radius);
+        transition:
+            filter var(--app-transition-duration-background),
+            background-color var(--app-transition-duration-background),
+            opacity var(--app-transition-duration-background);
     }
 
     &[data-type="plain"],
     &[data-type="light"] {
-        &[data-on-primary="true"] {
-            color: var(--app-primary-color);
-            background-color: var(--app-background-color-on-primary);
-            border-color: var(--app-background-color-on-primary);
+        &::before {
+            background-color: var(--mk-button-main-color);
+        }
+
+        @include on-primary {
+            &::before {
+                background-color: var(--app-background-color-on-primary);
+            }
         }
     }
 
     &[data-type="plain"] {
-        color: var(--mk-button-text-color);
-        background-color: var(--mk-button-background-color);
-        border-color: var(--mk-button-background-color);
+        @mixin plain-hover-active-state {
+            &::before {
+                filter: brightness(90%);
+            }
+        }
+
+        @include not-disabled {
+            &:hover, {
+                @include plain-hover-active-state;
+            }
+
+            &:active {
+                &::before {
+                    filter: brightness(80%);
+                }
+            }
+        }
+
+        @include is-active {
+            @include plain-hover-active-state;
+        }
+
+        @include on-classic {
+            color: var(--mk-button-text-color-on-main-color);
+        }
+
+        @include on-primary {
+            color: var(--app-text-color-on-background-color-on-primary);
+        }
     }
 
     &[data-type="light"] {
-        color: var(--mk-button-light-text-color);
-        background-color: var(--mk-button-light-background-color);
-        border-color: var(--mk-button-light-background-color);
-    }
+        @mixin light-hover-active-state {
+            @include light {
+                &::before {
+                    opacity: 0.2;
+                }
+            }
 
-    &[data-type="outlined"],
-    &[data-type="text"] {
-        color: var(--mk-button-hollowed-text-color);
-        background-color: transparent;
+            @include dark {
+                &::before {
+                    opacity: 0.4;
+                }
+            }
+        }
 
-        &[data-on-primary="true"] {
+        @include on-classic {
+            @include icon-color;
+
+            color: var(--mk-button-text-color);
+
+            @include light {
+                &::before {
+                    opacity: 0.1;
+                }
+            }
+
+            @include dark {
+                &::before {
+                    opacity: 0.35;
+                }
+            }
+
+            @include not-disabled {
+                &:hover {
+                    @include light-hover-active-state;
+                }
+
+                &:active {
+                    @include light {
+                        &::before {
+                            opacity: 0.4;
+                        }
+                    }
+
+                    @include dark {
+                        &::before {
+                            opacity: 0.5;
+                        }
+                    }
+                }
+            }
+
+            @include is-active {
+                @include light-hover-active-state;
+            }
+        }
+
+        @mixin light-on-primary-hover-active-state {
+            &::before {
+                opacity: 0.2;
+            }
+        }
+
+        @include on-primary {
             color: var(--app-text-color-on-primary);
+
+            &::before {
+                opacity: 0.1;
+            }
+
+            @include not-disabled {
+                &:hover {
+                    @include light-on-primary-hover-active-state;
+                }
+
+                &:active {
+                    &::before {
+                        opacity: 0.4;
+                    }
+                }
+            }
+
+            @include is-active {
+                @include light-on-primary-hover-active-state;
+            }
         }
     }
 
     &[data-type="text"] {
-        border-color: transparent;
+        color: var(--mk-button-text-color);
+
+        @include content {
+            border-color: transparent;
+        }
+
+        @mixin text-hover-active-state {
+            color: var(--mk-button-main-color);
+
+            .mk-AppIcon {
+                --mk-icon-color: var(--mk-button-main-color);
+            }
+        }
+
+        @include on-classic {
+            @include icon-color;
+
+            @include not-disabled {
+                &:hover {
+                    @include text-hover-active-state;
+                }
+            }
+
+            @include is-active {
+                @include text-hover-active-state;
+            }
+
+            @include on-primary {
+                color: var(--app-text-color-on-primary);
+            }
+        }
     }
 
     &[data-type="outlined"] {
-        padding: var(--mk-button-padding-y) var(--mk-button-padding-x);
-
-        &[data-size="wide"] {
-            padding: var(--mk-button-wide-padding-y) var(--mk-button-wide-padding-x);
+        @include content {
+            padding: var(--mk-button-bordered-padding-y) var(--mk-button-bordered-padding-x);
+            border-style: solid;
+            border-width: var(--mk-button-border-width);
         }
 
-        &[data-on-primary="true"] {
-            border-color: var(--app-background-color-on-primary);
+        @mixin outlined-hover-active-state {
+            @include content {
+                border-color: var(--mk-button-main-color);
+            }
+        }
+
+        @include on-classic {
+            @include icon-color;
+
+            color: var(--mk-button-text-color);
+
+            @include content {
+                border-color: var(--mk-button-border-color);
+            }
+
+            &::before {
+                background-color: var(--mk-button-main-color);
+                opacity: 0;
+            }
+
+            @include not-disabled {
+                &:hover {
+                    @include outlined-hover-active-state;
+                }
+
+                &:active {
+                    @include light {
+                        &::before {
+                            opacity: 0.4;
+                        }
+                    }
+
+                    @include dark {
+                        &::before {
+                            opacity: 0.5;
+                        }
+                    }
+                }
+            }
+
+            @include is-active {
+                @include outlined-hover-active-state;
+
+                @include light {
+                    &::before {
+                        opacity: 0.15;
+                    }
+                }
+
+                @include dark {
+                    &::before {
+                        opacity: 0.3;
+                    }
+                }
+            }
+        }
+
+        @mixin outlined-on-primary-hover-active-state {
+            @include content {
+                border-color: var(--app-background-color-on-primary);
+            }
+
+            &::before {
+                opacity: 0.2;
+            }
+        }
+
+        @include on-primary {
+            color: var(--app-text-color-on-primary);
+
+            @include content {
+                border-color: var(--app-background-color-on-primary);
+            }
+
+            &::before {
+                background-color: var(--app-background-color-on-primary);
+                opacity: 0;
+            }
+
+            @include not-disabled {
+                &:hover {
+                    @include outlined-on-primary-hover-active-state;
+                }
+
+                &:active {
+                    &::before {
+                        opacity: 0.4;
+                    }
+                }
+            }
+
+            @include is-active {
+                @include outlined-on-primary-hover-active-state;
+            }
+        }
+    }
+
+    &[data-size="wide"] {
+        font-size: var(--mk-button-wide-font-size);
+
+        @include content {
+            padding: var(--mk-button-wide-padding-y) var(--mk-button-wide-padding-x);
         }
     }
 
